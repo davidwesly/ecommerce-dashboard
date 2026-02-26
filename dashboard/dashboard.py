d2c09bae3a85c6d5240af0e796c467c64190f3a9
davidwesly <david.nata2120@gmail.com>
2026-02-27 02:00:04 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -8,11 +8,9 @@ st.set_page_config(page_title="E-Commerce Dashboard", layout="wide")
 # Load data
 @st.cache_data
 def load_data():
-    st.write("Current working directory:", os.getcwd())
-    st.write("Files in directory:", os.listdir())
-    df = pd.read_csv("main_data.csv")
-    df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])
-    return df
+   df = pd.read_csv("dashboard/main_data.csv")
+   df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])
+   return df
 
 df = load_data()
 
@@ -181,6 +179,7 @@ ax5.set_ylabel("Number of Customers")
 
 
 st.pyplot(fig5)
+
 
 
 
