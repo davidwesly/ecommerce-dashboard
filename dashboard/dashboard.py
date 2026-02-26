4e8cb8727bcf35950c12868443b872233f6d9a1d
davidwesly <david.nata2120@gmail.com>
2026-02-27 01:39:49 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="E-Commerce Dashboard", layout="wide")
 # Load data
 @st.cache_data
 def load_data():
-    df = pd.read_csv("dashboard/main_data.csv")
+    df = pd.read_csv("main_data.csv")
     df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])
     return df
 
@@ -178,3 +178,4 @@ ax5.set_ylabel("Number of Customers")
 
 
 st.pyplot(fig5)
+
